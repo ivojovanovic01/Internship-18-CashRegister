5711bc4d9c1bbd16dd5a9d9cd3db6b0d7b1ac0d4
ivojovanovic01 <ivo.jovanovic2@gmail.com>
2019-06-26 11:47:47 +0200

    Created ProductController

---
+++ CashRegister.Web/Controllers/ProductController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CashRegister.Data.Entities.Models;
+using CashRegister.Domain.Repositories.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CashRegister.Web.Controllers
+{
+    [Route("api/products")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        public ProductController(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+        private readonly IProductRepository _productRepository;
+
+        [HttpGet("all")]
+        public IActionResult GetAllProducts()
+        {
+            return Ok(_productRepository.GetAllProducts());
+        }
+
+        [HttpPost("add")]
+        public IActionResult AddProduct(Product productToAdd)
+        {
+            var wasAddSuccessful = _productRepository.AddProduct(productToAdd);
+
+            if (wasAddSuccessful)
+                return Ok();
+            return Forbid();
+        }
+
+        [HttpPost("edit")]
+        public IActionResult EditProduct(Product editedProduct)
+        {
+            var wasEditSuccessful = _productRepository.EditProduct(editedProduct);
+            if (wasEditSuccessful)
+                return Ok();
+            return NotFound();
+        }
+
+        [HttpGet("get-by-id")]
+        public IActionResult GetProductById(int id)
+        {
+            var product = _productRepository.GetProductById(id);
+            if (product != null)
+                return Ok(product);
+            return NotFound();
+        }
+    }
+}
